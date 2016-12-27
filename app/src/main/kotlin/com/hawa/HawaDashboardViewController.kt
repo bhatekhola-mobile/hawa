6761265b9a4c20080923de3ad5c7ba20dc424ad1
prayagupd <prayag.upd@gmail.com>
2016-12-27 00:55:12 -0800

    add new view to be shown from drawerlayout

--- app/src/main/kotlin/com/hawa/HawaDashboardViewController.kt
+++ app/src/main/kotlin/com/hawa/HawaDashboardViewController.kt
@@ -1,40 +1,59 @@
-package hawa.com.hawa
+package com.hawa
 
+import android.app.Fragment
+import android.app.FragmentManager
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
-import android.view.View
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
+import android.support.v4.widget.NestedScrollView
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
+import android.util.Log
+import android.view.*
+import android.widget.FrameLayout
+import android.widget.SlidingDrawer
+import com.hawa.R
 
-class HawaDashboardViewController : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class HawaDashboardViewController : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hawa_dashboard_view)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
+        setupFab()
+
+        setupSlidingMenu()
+    }
+
+    private fun setupFab() {
+        val floatingActionButton = findViewById(R.id.fab) as FloatingActionButton
+
+        floatingActionButton.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
 
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
-        val toggle = ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer.setDrawerListener(toggle)
-        toggle.syncState()
+    private fun setupSlidingMenu() {
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val drawerLayout = findViewById(R.id.drawer_layout) as DrawerLayout
+//        drawerLayout.isClickable = true
+
+        val openCloseToggle = ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(openCloseToggle)
+
+        openCloseToggle.syncState()
 
         val navigationView = findViewById(R.id.nav_view) as NavigationView
-        navigationView.setNavigationItemSelectedListener(this)
+        navigationView.setNavigationItemSelectedListener { onNavigationItemSelected(it) }
+
     }
 
     override fun onBackPressed() {
@@ -58,7 +77,6 @@ class HawaDashboardViewController : AppCompatActivity(), NavigationView.OnNaviga
         // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
 
-
         if (id == R.id.action_settings) {
             return true
         }
@@ -66,14 +84,23 @@ class HawaDashboardViewController : AppCompatActivity(), NavigationView.OnNaviga
         return super.onOptionsItemSelected(item)
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+    fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         val id = item.itemId
+        Log.i("HawaDashboardView", ""+id)
+
+        var switchView : Fragment
 
         if (id == R.id.study) {
+            Log.i("HawaDashboardView", ""+id)
             // show topics
             // questions and
             // answers stuff
+            switchView = StudyView()
+
+            fragmentManager.beginTransaction()
+                    .replace(R.id.frame_container, switchView).commit()
+
         } else if (id == R.id.old) {
 
         } else if (id == R.id.practise) {
@@ -90,4 +117,15 @@ class HawaDashboardViewController : AppCompatActivity(), NavigationView.OnNaviga
         drawer.closeDrawer(GravityCompat.START)
         return true
     }
+
+    class StudyView : Fragment() {
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View {
+
+            val rootView = inflater.inflate(R.layout.study_view, container, false)
+
+            return super.onCreateView(inflater, container, savedInstanceState)
+        }
+    }
+
 }
---
+++ app/src/main/kotlin/com/hawa/StudyViewController.kt
@@ -0,0 +1,12 @@
+package com.hawa
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StudyViewController : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.study_view)
+    }
+}
